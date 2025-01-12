2e7ac28afbed2be6c344978b00dcdd04b61e022c
Anastasiia Chaika <chaika.anastasiia@lll.kpi.ua>
2025-01-12 19:13:47 +0200

    Delete FinishedTasks/between.py

--- FinishedTasks/between.py
+++
@@ -1,9 +0,0 @@
-def compare(first_values, second_values):
-
-    a = list(first_values.keys())
-    b = list(second_values.keys())
-
-    if a != b:
-        return False
-    return all(first_values[c] == second_values[c] for c in a)
-
