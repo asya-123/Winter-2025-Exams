3af0afa86a42083d5913590f507c2f5c5e889544
asya-123 <chaika.anastasiia@lll.kpi.ua>
2025-01-13 17:32:09 +0200

    finished task day

--- FinishedTasks/day.py
+++ FinishedTasks/day.py
@@ -1,3 +1,7 @@
-dict_with_days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+dict_days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 
-
+def parse_day(input_string):
+    for index, day in enumerate(dict_days, 1):
+        if input_string.lower().startswith(day.lower()):
+            return index
+    return -1
